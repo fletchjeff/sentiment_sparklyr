95b92122eee45e875792b509350c055a1ddf4bc8
Jeff Fletcher <jfletcher@cloudera.com>
2020-09-21 12:20:35 +0000

    Working updates

--- sentiment.R
+++ sentiment.R
@@ -27,13 +27,21 @@ config$spark.yarn.access.hadoopFileSystems <- storage
 sc <- spark_connect(master = "yarn-client", config=config)
 
 #### Read in the CSV data
-# The data is very simple dataset. Its 2 columns, on
+# The text data is a very simple dataset. Its 2 columns, one for the character, and on for their
+# dialog. Since we know that its 2 columns of `characters` we will set the schema and save
+# spark the trouble of doing it automatically.
+
+#```
+#Miss Hoover,"No, actually, it was a little of both. Sometimes when a disease is in all he magazines and all the news shows, it's only natural that you think you have it."
+#Lisa Simpson,Where's Mr. Bergstrom?
+#Miss Hoover,I don't know. Although I'd sure like to talk to him. He didn't touch my lesson plan. What did he teach you?
+#Lisa Simpson,That life is worth living.
+#```
+
 cols = list(
   raw_character_text = "character",
   spoken_words = "character"
 )
-
-# The first dataset is the 
 
 spark_read_csv(
   sc,
@@ -43,6 +51,18 @@ spark_read_csv(
   columns = cols,
   header = TRUE
 )
+
+
+
+# The other dataset we will use is the AFINN list. https://github.com/fnielsen/afinn/tree/master/afinn/data 
+# its 2 column, the first being the word and the second and integer value for its `valance`. 
+
+#```
+#abandon	-2
+#abandoned	-2
+#abandons	-2
+#abducted	-2
+#```
 
 spark_read_csv(
   sc,
@@ -143,36 +163,84 @@ sentences <- simpsons_spark_table %>%
   filter(nchar(word) > 2) %>% 
   compute("simpsons_spark_table")
 
+#sentence_values <- sentences %>% 
+#  inner_join(afinn_table) %>% 
+#  group_by(spoken_words) %>% 
+#  summarise(weighted_sum = sum(value)/count())
+
 sentence_values <- sentences %>% 
   inner_join(afinn_table) %>% 
   group_by(spoken_words) %>% 
-  summarise(weighted_sum = sum(value)/count())
+  summarise(weighted_sum = sum(value))
+
+weighted_sum_summary <- sentence_values %>% sdf_describe(cols="weighted_sum")
+
+weighted_sum_summary
+
+weighted_sum_mean <- as.data.frame(weighted_sum_summary)[2,2]
+
+sentence_values %>% 
+  mutate(sent_score = ifelse(weighted_sum > weighted_sum_mean,1,0))
 
 sentence_values_tokenized <- 
-  sentence_values %>% 
+  sentence_scores %>% 
   ft_tokenizer(input_col="spoken_words",output_col= "word_list") %>%
   ft_stop_words_remover(input_col = "word_list", output_col = "wo_stop_words")
 
-
-w2v <- ft_word2vec(sentence_values_tokenized, input_col = "wo_stop_words", output_col = "result", min_count = 2)
-
-w2v_model <- ft_word2vec(sc, input_col = "wo_stop_words", output_col = "result", min_count = 2)
+w2v_model <- ft_word2vec(sc, 
+                         input_col = "wo_stop_words", 
+                         output_col = "result", 
+                         #min_count = 5,
+                         #max_iter = 25,
+                         #vector_size = 400,
+                         #step_size = 0.0125
+                        )
 
 w2v_model_fitted <- ml_fit(w2v_model,sentence_values_tokenized)
 
-ml_find_synonyms(w2v_model_fitted,"doh",2)
+w2v_model_fitted <- ml_load(sc,"s3a://demo-aws-1/datalake/data/sentiment/w2v_model_fitted")
+
+
+#ml_find_synonyms(w2v_model_fitted,"doh",2)
 
 w2v_transformed <- ml_transform(w2v_model_fitted, sentence_values_tokenized)
 
-lm_model <- w2v_transformed %>% ml_linear_regression(weighted_sum ~ result)
-
-pred <- ml_predict(lm_model, w2v_transformed)
-
-ml_regression_evaluator(pred, label_col = "weighted_sum",
-                        prediction_col = "prediction", metric_name = "rmse")
+w2v_transformed <- w2v_transformed %>% mutate(sent_score = ifelse(weighted_sum>mean,1,0))
 
 
-pred %>% select(weighted_sum,prediction) %>% sdf_describe()
+w2v_transformed_split <- w2v_transformed %>% sdf_random_split(training=0.7, test = 0.3)
+
+maxIter= 500
+elasticNetParam = 0.0
+regParam = 0.01
+
+lr_model <- w2v_transformed_split$training %>% select(result,sent_score) %>% 
+  ml_logistic_regression(
+    sent_score ~ result,
+    #max_iter=maxIter, 
+    #elastic_net_param=elasticNetParam,
+    #reg_param = regParam
+  )
+
+
+density_plot <- function(X) {
+  hist(X, prob=TRUE, col="grey", breaks=500, xlim=c(-10,10), ylim=c(0,0.2))# prob=TRUE for probabilities not counts
+  lines(density(X), col="blue", lwd=2) # add a density estimate with defaults
+  #lines(density(X, adjust=2), lty="dotted", col="darkgreen", lwd=2) 
+}
+
+density_plot(as.data.frame(w2v_transformed %>% select(weighted_sum))$weighted_sum)
+
+
+pred_lr_training <- ml_predict(lr_model, w2v_transformed_split$training)
+
+pred_lr_test<- ml_predict(lr_model, w2v_transformed_split$test)
+
+ml_binary_classification_evaluator(pred_lr_training,label_col = "sent_score",
+                        prediction_col = "prediction", metric_name = "areaUnderROC")
+
+ml_binary_classification_evaluator(pred_lr_test,label_col = "sent_score",
+                        prediction_col = "prediction", metric_name = "areaUnderROC")
 
 ## Creating a reusable model pipeline.
 
@@ -180,7 +248,7 @@ sentiment_pipeline <- ml_pipeline(sc) %>%
   ft_tokenizer(input_col="spoken_words",output_col= "word_list") %>%
   ft_stop_words_remover(input_col = "word_list", output_col = "wo_stop_words") %>%
   ft_word2vec(input_col = "wo_stop_words", output_col = "result", min_count = 10) %>%
-  ft_r_formula(weighted_sum ~ result) %>% 
+  ft_r_formula(sent_score ~ result) %>% 
   ml_linear_regression()
 
 sentiment_model <- ml_fit(sentiment_pipeline,sentence_values)
